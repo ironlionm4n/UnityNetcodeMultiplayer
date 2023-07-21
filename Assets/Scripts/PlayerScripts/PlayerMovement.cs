c5d65dc1a9d7ab04ebfa9a3a3e362e54505ae033
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-07-21 09:23:47 -0400

    rotating treads, move in forward direction, background sprite

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Input;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Scripts.PlayerScripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : NetworkBehaviour
     {
-        
-    }
+        [Header("Tank Movement References & Settings")] [SerializeField]
+        private InputReader inputReader;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private Transform tankTreads;
+        [SerializeField] private Rigidbody2D tankRigidbody;
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float turnRotationSpeed;
+
+        private Vector2 currentMovementInput;
+
+        private void Update()
+        {
+            if (!IsOwner) return;
+            
+            tankTreads.Rotate(0f, 0f, currentMovementInput.x * (-turnRotationSpeed * Time.deltaTime));
+        }
+
+        private void FixedUpdate()
+        {
+            if (!IsOwner) return;
+
+            tankRigidbody.velocity = (Vector2) tankTreads.up * (movementSpeed * currentMovementInput.y);
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (!IsOwner) return;
+            base.OnNetworkSpawn();
+            inputReader.PlayerMoving += HandleMove;
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (!IsOwner) return;
+            base.OnNetworkDespawn();
+            inputReader.PlayerMoving -= HandleMove;
+        }
+
+        private void HandleMove(Vector2 movement)
+        {
+            currentMovementInput = movement;
+        }
     }
 }
